01f45ad41054a59a7e369f2d2830cf399cf15994
Abir Hasan <cse.170201063@gmail.com>
2023-01-10 20:27:19 +0600

    Tuto-11 Navigation programmatically

--- Next-routing/pages/index.js
+++ Next-routing/pages/index.js
@@ -1,6 +1,12 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 function Home() {
+    const router = useRouter()
+    const handlleClick = () => {
+        // router.push('/Product')
+        router.replace('/Product')
+    }
   return (
     <div>
       <h1>Home Page</h1>
@@ -12,7 +18,8 @@ function Home() {
       </Link>
       <Link href="/docs">
         Docs
-      </Link>
+          </Link>
+          <button onClick={handlleClick}>Please Order</button>
     </div>
   );
 }
