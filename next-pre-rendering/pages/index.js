22741dc77f77a8be5a09b908c205043ebc6fdfee
Abir Hasan <cse.170201063@gmail.com>
2023-01-11 20:21:01 +0600

    Tuto 21 Running static generation builds

--- next-pre-rendering/pages/index.js
+++ next-pre-rendering/pages/index.js
@@ -1,5 +1,12 @@
+import Link from 'next/link'
 function Home() {
-  return <h1>Home Next js pre rendering</h1>
+  return (
+    <>
+      <h1>Home Next js pre rendering</h1>
+      <Link href="/users">
+      Users</Link>
+    </>
+  );
 }
 
 export default Home
