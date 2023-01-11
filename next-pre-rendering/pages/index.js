6b9611e30ef46c33d40976406d0f660f6cde1752
Abir Hasan <cse.170201063@gmail.com>
2023-01-11 18:03:22 +0600

    tuto 17 static generation with getStaticProps

---
+++ next-pre-rendering/pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+  return <h1>Home Next js pre rendering</h1>
+}
+
+export default Home
---
+++ next-pre-rendering/pages/users.js
@@ -0,0 +1,27 @@
+function UserList({ users }) {
+  return (
+    <>
+      <h1>List of User</h1>
+      {users.map((user) => {
+        return (
+          <div key={user.id}>
+            <p>{user.name}</p>
+            <p>{user.email}</p>
+          </div>
+        );
+      })}
+    </>
+  );
+}
+export default UserList;
+
+export async function getStaticProps() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  console.log(data);
+  return {
+    props: {
+      users: data,
+    },
+  };
+}
