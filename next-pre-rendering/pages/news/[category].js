5b65ef3ba88e326f9072161bcd5df535a55c1f3e
Abir Hasan <cse.170201063@gmail.com>
2023-01-13 04:04:55 +0600

    Tuto 33 SSR with dynamic parameters

---
+++ next-pre-rendering/pages/news/[category].js
@@ -0,0 +1,31 @@
+function ArticleListByCategory({articles,category}) {
+    return (
+        <>
+            <h1>Showing news for category {category}</h1>
+            {
+                articles.map(article => {
+                    return (
+                        <div key={article.id}>
+                            <h2>{article.id} {article.title}</h2>
+                            <p>{ article.description }</p>
+                        </div>
+                    )
+                })
+            }
+        </>
+    )
+}
+export default ArticleListByCategory
+
+export async function getServerSideProps(context) {
+    const { params } = context
+    const {category}=params
+    const res = await fetch(`http://localhost:4000/news?category=${category}`);
+    const data = await res.json();
+    return {
+        props: {
+            articles: data,
+            category,
+        }
+    }
+}
