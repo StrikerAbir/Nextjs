5121f3f6e0037b152e01f100bbd4c5ac2856fc4c
Abir Hasan <cse.170201063@gmail.com>
2023-01-16 05:00:58 +0600

    Tuto 43 Post request

--- next-api/pages/comments/index.js
+++ next-api/pages/comments/index.js
@@ -2,14 +2,30 @@ import { useState } from "react";
 
 function CommentPage() {
     const [comments,setComments] =useState([])
+    const [comment, setComment] = useState('')
+    
     const fetchComs = async() => {
         const response = await fetch('/api/comments');
         const data = await response.json();
         console.log(data);
         setComments(data)
     }
+
+    // console.log(comment);
+    const submitCom = async () => {
+        const response = await fetch("/api/comments", {
+            method: "POST",
+            headers: {'Content-Type':'application/json'},
+            body:JSON.stringify({comment})
+        });
+        const data = await response.json();
+        console.log(data);
+        
+    }
     return (
         <>
+            <input type="text" value={comment} onChange={e => setComment(e.target.value)} />
+            <button onClick={submitCom}>Submit</button>
             <button onClick={fetchComs}>Load comments</button>
             {
                 comments?.map(comment => {
